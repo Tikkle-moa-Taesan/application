2e243fe385715b280b0bfa0c0fe6b9d1ba956006
김태연 <xodus4194@gmail.com>
2024-11-05 18:31:47 +0900

    Initial commit for BE_develop branch

---
+++ src/main/java/com/ssafy/TmT/TmTApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.TmT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmTApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssafy/TmT/TmTApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.TmT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmTApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
