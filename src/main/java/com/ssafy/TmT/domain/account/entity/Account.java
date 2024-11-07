b6e50ab5abed950f36878e001e34750d46d49189
김태연 <xodus4194@gmail.com>
2024-11-07 17:58:35 +0900

    1107 done

--- src/main/java/com/ssafy/TmT/domain/account/entity/Account.java
+++ src/main/java/com/ssafy/TmT/domain/account/entity/Account.java
@@ -1,9 +1,11 @@
 package com.ssafy.TmT.domain.account.entity;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import com.ssafy.TmT.domain.member.entity.Member;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -12,6 +14,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -30,13 +33,17 @@ public class Account {
     @Column(name = "account_number", length = 20, nullable = false) // 컬럼 길이 및 null 허용 여부 지정
     private String accountNumber;
 
-    @Column(name = "bank_name", length = 50, nullable = false)
-    private String bankName;
-
     @Column(name = "balance", nullable = false)
     private BigDecimal balance;
 
     @ManyToOne(fetch = FetchType.LAZY)	// 조회할때만 갱신
     @JoinColumn(name = "member_id") // 외래 키 이름 명시
     private Member member;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "bank_id")
+    private Bank bank;
+    
+    @OneToMany(mappedBy = "account", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Transaction> transactions;
 }
---
+++ src/main/java/com/ssafy/TmT/domain/account/entity/Category.java
@@ -0,0 +1,29 @@
+package com.ssafy.TmT.domain.account.entity;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonProperty("category_id")
+    private Long categoryId;
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("type")
+    private String type; // 수입 또는 지출 구분
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Transaction> transactions;
+}
