3acd96a44573656dca07542dfc814ef07ecdcff6
김태연 <xodus4194@gmail.com>
2024-11-19 17:59:44 +0900

    fix swagger

--- src/main/java/com/ssafy/TmT/config/SecurityConfig.java
+++ src/main/java/com/ssafy/TmT/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
             .httpBasic(basic -> basic.disable())
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/oauth/**","/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                .requestMatchers("/api/oauth/**","/api/swagger-ui/**", "/api/v3/api-docs/**").permitAll()
                 .anyRequest().authenticated())
             .addFilterBefore(new JwtAuthenticationFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
 
