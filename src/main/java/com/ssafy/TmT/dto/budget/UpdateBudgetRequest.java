608c064b848a58bffd58f0e3f81d8c04344e529c
tyHome <xodus4194@gmail.com>
2024-11-27 01:25:52 +0900

    update put budget2

--- src/main/java/com/ssafy/TmT/dto/budget/UpdateBudgetRequest.java
+++ src/main/java/com/ssafy/TmT/dto/budget/UpdateBudgetRequest.java
@@ -2,10 +2,12 @@ package com.ssafy.TmT.dto.budget;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class UpdateBudgetRequest {
 
